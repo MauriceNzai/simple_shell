d240646169302f8147ae4536fc9c64c02b1caca2
wangenga <daisyimg65@gmail.com>
2022-09-17 12:28:40 -0700

    add executor.h

---
+++ executor.h
@@ -0,0 +1,14 @@
+#ifndef EXECUTOR_H
+
+#define EXECUTOR_H
+
+
+#include "node.h"
+
+
+char *search_path(char *file);
+int do_exec_cmd(int argc, char **argv);
+int do_simple_command(struct node_s *node);
+
+
+#endif
